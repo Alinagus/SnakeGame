52302e9f587a66acf9286bd6922a500afc8ede95
Alinagus <124495073+Alinagus@users.noreply.github.com>
2024-10-07 14:50:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,145 +1,134 @@
-let gameField = document.getElementById("game-field");
-let bestScoreBox = document.getElementById("best-score")
-
-const snakeSpeed = 150;
-let score = 0;
-let apple = { 
-  x: 20, 
-  y: 10 
-};
-
-class fieldSize{
+class Game{
   constructor(FieldSize, squareSize){
-    this._FieldSize = FieldSize
-    this._squareSize = squareSize
-    this.numOfSquare = this._FieldSize / this._squareSize
-  }
-}
-
-const field1 = new fieldSize(500, 20)
-bestResult();
-
-let snake = [
-  { x: 5, y: 4 },
-  { x: 4, y: 4 },
-  { x: 3, y: 4 },
-  { x: 2, y: 4 },
-];
-
-function drawSnake(){
-  for (let i = 0; i < snake.length; i++){
-  let snakeBody = `<div class="snake" style='left:${snake[i].x * field1._squareSize + "px"}; top: ${snake[i].y * field1._squareSize + "px"}'>`;
-  gameField.innerHTML += snakeBody;
-  }
-};
-
-function drawApple(){
-  let applePlace = `<div class="apple" style='left:${apple.x * field1._squareSize + "px"}; top: ${apple.y * field1._squareSize + "px"}'>`;
-  gameField.innerHTML += applePlace;
-};
-
-
-function clearElement() {
-  while (gameField.lastChild) {
-    gameField.removeChild(gameField.lastChild);
-  }
-
-}
-
-function updateScore() {
-  let countScore = document.getElementById("score");
-  countScore.textContent = "Счет: " + score;
-}
-
-
-function gameLoop() {
-  clearElement();
-  moveSnake();
-  drawSnake();
-  drawApple();
-  updateScore();
-}
-
-document.addEventListener("keydown", function (event) {
-  changeDirection(event.keyCode);
-});
-
-function changeDirection(keyCode) {
-  if (keyCode === 37 && direction !== "right") {
-    direction = "left";
-  } else if (keyCode === 38 && direction !== "down") {
-    direction = "up";
-  } else if (keyCode === 39 && direction !== "left") {
-    direction = "right";
-  } else if (keyCode === 40 && direction !== "up") {
-    direction = "down";
-  }
-}
-
-
-
-function moveSnake() {
-  let startMove = { x: snake[0].x, y: snake[0].y };
-  if (direction === "right") {
-    startMove.x++;
-  } else if (direction === "left") {
-    startMove.x--;
-  } else if (direction === "up") {
-    startMove.y--;
-  } else if (direction === "down") {
-    startMove.y++;
-  }
-
-  snake.unshift(startMove);
-
-  if (startMove.x === apple.x && startMove.y === apple.y) {
-    generateApple();
-    score += 1;
-  } else {
-    snake.pop();
-  }
-
-  for(let i = 0; i<snake.length; i++){
-    if (apple.x === snake[i].x && apple.y === snake[i].y){
-      generateApple();
-    }
+    this.gameField = document.getElementById("game-field");
+    this.bestScoreBox = document.getElementById("best-score");
+    this.snake = [
+      { x: 5, y: 4 },
+      { x: 4, y: 4 },
+      { x: 3, y: 4 },
+      { x: 2, y: 4 },
+    ];
+    this.squareSize = squareSize
+    this.numOfSquare = FieldSize / this.squareSize
+    this.snakeSpeed = 150;
+    this.score = 0;
+    this.apple = { 
+      x: 20, 
+      y: 10 
+    };
+    this.direction = "right";
+    this.event = new MouseEvent("keydown",  (event) => {
+      newGame.changeDirection(event.keyCode);
+     }); 
   } 
 
-  if (Collision()) {
-    alert(`Ваш счет: ${score}`);
-    saveResult();
-    resetGame();
-  }
-}
+    drawSnake(){
+      for (let i = 0; i < this.snake.length; i++){
+      let snakeBody = `<div class="snake" style='left:${this.snake[i].x * this.squareSize + "px"}; top: ${this.snake[i].y * this.squareSize + "px"}'>`;
+      this.gameField.innerHTML += snakeBody;
+      }
+    };
+      Collision() {
+        this.head = this.snake[0];
+        if (this.head.x >= this.numOfSquare || this.head.y >= this.numOfSquare || this.head.y < 0 || this.head.x < 0) {
+          return true;
+        };
+      
+        for (let i = 1; i < this.snake.length; i++) {
+          if (this.head.x === this.snake[i].x && this.head.y === this.snake[i].y) {
+            return true;
+          }
+        }
+        return false;
+      }
+      
+      moveSnake() {
+        let startMove = { x: this.snake[0].x, y: this.snake[0].y };
+        if (this.direction === "right") {
+          startMove.x++;
+        } else if (this.direction === "left") {
+          startMove.x--;
+        } else if (this.direction === "up") {
+          startMove.y--;
+        } else if (this.direction === "down") {
+          startMove.y++;
+        }
+      
+        this.snake.unshift(startMove);
+      
+        if (startMove.x === this.apple.x && startMove.y === this.apple.y) {
+          generateApple();
+          this.score += 1;
+        } else {
+          this.snake.pop();
+        }
+      
+        for(let i = 0; i<this.snake.length; i++){
+          if (this.apple.x === this.snake[i].x && this.apple.y === this.snake[i].y){
+            generateApple();
+          }
+        } 
+      
+        if (this.Collision()) {
+          alert(`Ваш счет: ${score}`);
+          saveResult();
+          resetGame();
+        }
+      }
 
-function generateApple() {
-      apple.x = Math.floor(Math.random() * (field1.numOfSquare));
-      apple.y = Math.floor(Math.random() * (field1.numOfSquare));
-  
-}
-
-function Collision() {
-  head = snake[0];
-  if (head.x >= field1.numOfSquare || head.y >= field1.numOfSquare || head.y < 0 || head.x < 0) {
-    return true;
+  drawApple(){
+    this.gameField.innerHTML += `<div class="apple" style='left:${this.apple.x * this.squareSize + "px"}; top: ${this.apple.y * this.squareSize + "px"}'>`;
   };
 
-  for (let i = 1; i < snake.length; i++) {
-    if (head.x === snake[i].x && head.y === snake[i].y) {
-      return true;
+
+  clearElement() {
+    while (this.gameField.lastChild) {
+      this.gameField.removeChild(this.gameField.lastChild);
+    }
+
+  }
+
+  updateScore() {
+    this.countScore  = document.getElementById("score");
+    this.countScore.textContent = "Счет: " + this.score;
+  }
+  clearElement() {
+    while (this.gameField.lastChild) {
+      this.gameField.removeChild(this.gameField.lastChild);
+    }
+
+  }
+
+  gameLoop() {
+    this.clearElement();
+    this.drawSnake();
+    this.drawApple();
+    this.updateScore();
+    this.moveSnake();
+  }
+
+
+  changeDirection(keyCode) {
+    if (keyCode === 37 && this.direction !== "right") {
+      direction = "left";
+    } else if (keyCode === 38 && this.direction !== "down") {
+      direction = "up";
+    } else if (keyCode === 39 && this.direction !== "left") {
+      direction = "right";
+    } else if (keyCode === 40 && this.direction !== "up") {
+      direction = "down";
     }
   }
-  return false;
-}
 
-function saveResult(){
-  if (score > bestScore){
-    localStorage.setItem("bestScore", score);
-    bestScore = localStorage;
-    bestScoreBox.textContent = "Лучший результат: " + score;
+
+
+  generateApple() {
+    this.apple.x = Math.floor(Math.random() * (field1.numOfSquare));
+    this.apple.y = Math.floor(Math.random() * (field1.numOfSquare));
+    
   }
-}
-  function bestResult(){
+
+  bestResult(){
     if (localStorage.getItem("bestScore")){
       bestScoreBox.textContent = "Лучший результат: " + localStorage.getItem("bestScore");
       bestScore = localStorage.getItem("bestScore");
@@ -150,19 +139,23 @@ function saveResult(){
     }
   }
 
+  saveResult(){
+    if (this.score > bestScore){
+      localStorage.setItem("bestScore", this.score);
+      bestScore = localStorage;
+      bestScoreBox.textContent = "Лучший результат: " + this.score;
+    }
+  }
 
-function resetGame() {
-  snake = [
-    { x: 5, y: 4 },
-    { x: 4, y: 4 },
-    { x: 3, y: 4 },
-    { x: 2, y: 4 },
-  ];
-  direction = "right";
-  generateApple();
-  score = 0;
+
+  resetGame() {
+    this.snake;
+    direction = "right";
+    generateApple();
+    this.score = 0;
+
+  }
 
 }
-
-let direction = "right";
-setInterval(gameLoop, snakeSpeed);
+let newGame = new Game(500, 20);
+setInterval(newGame.gameLoop, newGame.snakeSpeed);
